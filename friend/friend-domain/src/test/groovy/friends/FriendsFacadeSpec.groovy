9b8cb152fcf3889c1ec0e64bb708012ebf7c5a03
Michal Dabkowski <michal.dabkowski321@gmail.com>
2025-03-15 14:08:49 +0100

    SO-109 Add unit tests to friends domain

---
+++ friend/friend-domain/src/test/groovy/friends/FriendsFacadeSpec.groovy
@@ -0,0 +1,91 @@
+package friends
+
+import com.sedeo.friend.db.FriendRepository
+import com.sedeo.friend.facade.FriendsFacade
+import com.sedeo.friend.model.Friend
+import com.sedeo.friend.model.error.FriendError
+import io.vavr.control.Either
+import spock.lang.Specification
+
+class FriendsFacadeSpec extends Specification {
+
+    FriendRepository friendRepository = Mock()
+    FriendsFacade friendsFacade = new FriendsFacade(friendRepository)
+
+    def "fetchUsersPotentialFriends should return potential friends"() {
+        given: "a userId and search phrase"
+        UUID userId = UUID.randomUUID()
+        String searchPhrase = "John"
+
+        and: "the repository returns a list of potential friends"
+        List<Friend> friendsList = [new Friend(UUID.randomUUID(), "John", "Doe", "512770856")]
+        friendRepository.findUsersPotentialFriends(userId, searchPhrase) >> Either.right(friendsList)
+
+        when: "trying to fetch users potential friends"
+        def result = friendsFacade.fetchUsersPotentialFriends(userId, searchPhrase)
+
+        then: "the result should contain John only"
+        result.isRight()
+        result.get() == friendsList
+    }
+
+    def "createFriend should return an error if the friend already exists"() {
+        given: "a friend"
+        Friend friend = new Friend(UUID.randomUUID(), "Jane", "Doe", "347643189")
+
+        and: "the repository returns that the friend already exists"
+        friendRepository.friendExistsById(friend.userId()) >> true
+
+        when: "trying to create a new friend"
+        def result = friendsFacade.createFriend(friend)
+
+        then: "the result should be a FriendAlreadyExists error"
+        result.isLeft()
+        result.getLeft() instanceof FriendError.FriendAlreadyExists
+    }
+
+    def "createFriend should save the friend if they don't already exist"() {
+        given: "a friend"
+        Friend friend = new Friend(UUID.randomUUID(), "Jane", "Doe", "875345198")
+
+        and: "the repository returns that the friend does not exist"
+        friendRepository.friendExistsById(friend.userId()) >> false
+        friendRepository.save(friend) >> Either.right(friend)
+
+        when: "trying to create a new friend"
+        def result = friendsFacade.createFriend(friend)
+
+        then: "the friend should be saved successfully"
+        result.isRight()
+        result.get() == friend
+    }
+
+    def "fetchFriends should return an empty list if no userIds are provided"() {
+        given: "an empty list of userIds"
+        List<UUID> userIds = []
+
+        when: "trying to fetch friends"
+        def result = friendsFacade.fetchFriends(userIds)
+
+        then: "the result should be an empty list"
+        result.isRight()
+        result.get().isEmpty()
+    }
+
+    def "fetchFriends should return friends for valid userIds"() {
+        given: "a list of userIds"
+        List<UUID> userIds = [UUID.randomUUID(), UUID.randomUUID()]
+
+        and: "the repository returns a list of friends"
+        List<Friend> friendsList = [new Friend(UUID.randomUUID(), "John", "Doe", "578333567")]
+        friendRepository.findFriendsByIds(userIds) >> Either.right(friendsList)
+
+        when: "trying to fetch friends"
+        def result = friendsFacade.fetchFriends(userIds)
+
+        then: "the result should contain the list of friends"
+        result.isRight()
+        result.get() == friendsList
+    }
+}
+
---
+++ friend/friend-domain/src/test/groovy/friendships/FriendshipsFacadeSpec.groovy
@@ -0,0 +1,67 @@
+package friendships
+
+import com.sedeo.friend.db.FriendshipRepository
+import com.sedeo.friend.facade.FriendshipsFacade
+import com.sedeo.friend.model.Friendship
+import com.sedeo.friend.model.error.FriendError
+import io.vavr.control.Either
+import spock.lang.Specification
+
+class FriendshipsFacadeSpec extends Specification {
+
+    def friendshipRepository = Mock(FriendshipRepository)
+    def friendshipsFacade = new FriendshipsFacade(friendshipRepository)
+
+    def "createFriendship should create a new friendship when it does not exist"() {
+        given: "two ids of users who are not friends yet"
+        def userId1 = UUID.randomUUID()
+        def userId2 = UUID.randomUUID()
+        def friendship = Friendship.withRandomId(userId1, userId2)
+
+        and: "the repository returns false that the friendship already exists and returns friendship upon saving"
+        1 * friendshipRepository.friendshipExists(userId1, userId2) >> false
+        1 * friendshipRepository.save(_) >> Either.right(friendship)
+
+        when: "trying to create a friendship"
+        def result = friendshipsFacade.createFriendship(userId1, userId2)
+
+        then: "friendship should be created successfully"
+        result.isRight()
+        result.get() == friendship
+    }
+
+    def "createFriendship should return error when friendship already exists"() {
+        given: "two ids of users who are already friends"
+        def userId1 = UUID.randomUUID()
+        def userId2 = UUID.randomUUID()
+
+        and: "the repository returns true that the friendship already exists and does not save it"
+        1 * friendshipRepository.friendshipExists(userId1, userId2) >> true
+        0 * friendshipRepository.save(_)
+
+        when: "trying to create a friendship"
+        def result = friendshipsFacade.createFriendship(userId1, userId2)
+
+        then: "an error should be returned"
+        result.isLeft()
+        result.getLeft() instanceof FriendError.FriendsAlreadyExist
+    }
+
+    def "fetchUsersFriendships should fetch user's friendships successfully"() {
+        given: "id of a user with existing friendships"
+        def userId = UUID.randomUUID()
+        def friendship1 = Friendship.withRandomId(userId, UUID.randomUUID())
+        def friendship2 = Friendship.withRandomId(userId, UUID.randomUUID())
+        def friendships = [friendship1, friendship2]
+
+        and: "the repository returns users friendships"
+        1 * friendshipRepository.findUsersFriendships(userId) >> Either.right(friendships)
+
+        when: "trying to fetch users friendships"
+        def result = friendshipsFacade.fetchUsersFriendships(userId)
+
+        then: "users friendships should be returned correctly"
+        result.isRight()
+        result.get() == friendships
+    }
+}
